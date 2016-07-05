7ca27726400d92a9eca4197bd476e9b2d46c903d
setare karimi <str72.karimi@gmail.com>
2016-07-05 22:19:01 +0430

    add joqd framework

---
+++ footer.php
@@ -0,0 +1,9 @@
+		<!-- Scripts -->
+		<!-- Latest jQuery (CND) -->
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+		<!-- Joqd Core js -->
+		<script src="js/joqd.min.js"></script>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fa">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<meta charset="<?php bloginfo('charset'); ?>">
+
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+		<meta name="author" content="IKA">
+
+		<title>جغد، اولین فریم وورک ایرانی</title>
+
+		<!-- Styles -->
+		<!-- Joqd Core CSS -->
+		<link href="css/joqd.min.css" rel="stylesheet">
+
+		<!-- Gorbeh Icons -->
+		<link href="gorbeh/css/gorbeh-icons.min.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="css/custom.css" rel="stylesheet">
+
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
